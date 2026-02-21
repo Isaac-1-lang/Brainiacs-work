d50203bf6a7cdbbeb379fb794ad84a2bb06ce492
NIYOBYOSE Isaac Precieux <isaprecieux112@gmail.com>
2026-02-21 11:19:09 +0200

    Refactor prime number logic into separate function

--- brainiacs.c
+++ brainiacs.c
@@ -1,17 +1,25 @@
 #include <stdio.h>
-int main() {
-    int i, n, found;
+
+void printPrimeNumber() {
+    int n, i, found;
+
     for (n = 2; n <= 100; n++) {
-        found = 1; // Assume n is prime
-        for (i = 2; i < n; i++) {
+        found = 1;   // assume n is prime
+
+        for (i = 2; i * i <= n; i++) {
             if (n % i == 0) {
-                found = 0; // Not prime
+                found = 0;
                 break;
             }
         }
+
         if (found == 1) {
             printf("%d\n", n);
         }
     }
+}
+
+int main() {
+    printPrimeNumber();
     return 0;
 }
